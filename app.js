72d7671da34f834eab219dbbadfbf92590954bcf
Gary Janes <gary_janes@hotmail.com>
2018-04-25 08:54:48 +0100

    Remove unused server side rendering engine

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //  - module.exports
 // https://blog.risingstack.com/node-js-at-scale-module-system-commonjs-require/
 
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -25,8 +25,8 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
 // Wire up middlewares for route handlers
 app.use(bodyParser.json());
@@ -51,24 +51,23 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
+// app.use(function(req, res, next) {
+//     next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
 // Set up routes
 // Required file exports a function which we then call with app as parameter
