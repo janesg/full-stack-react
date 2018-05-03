05f8737e46f5c60fd87401c1a38ed45f4866e549
Gary Janes <gary_janes@hotmail.com>
2018-05-03 08:20:14 +0100

    Completed server-side implementation for survey creation and send

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,14 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// Temporary test code for exercising Mailer
+import axios from 'axios';
+window.axios = axios;
+
+// Type following directly into browser console to create and send email:
+// const survey = { title: 'my title', subject: 'my subject', recipients: 'gary.janes065@gmail.com', body: 'This is the Body of the email' };
+// axios.post('/api/surveys', survey);
+
 // Params for createStore: array of reducers, initial state, middlewares
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
--- routes/survey.js
+++ routes/survey.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const hasCredit = require('../middlewares/hasCredit');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 // Testing frameworks can sometimes experience issues
 // if same mongoose schema is required multiple times
@@ -8,7 +10,11 @@ const hasCredit = require('../middlewares/hasCredit');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, hasCredit, (req, res) => {
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thank you voting !');
+    });
+
+    app.post('/api/surveys', requireLogin, hasCredit, async (req, res) => {
         // Using ES6 destructuring
         const { title, subject, body, recipients } = req.body;
 
@@ -22,5 +28,27 @@ module.exports = app => {
             _user: req.user.id,     // Mongoose-generated id
             dateSent: Date.now()
         });
+
+        // Send an email !!!
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+
+        try {
+            // Async function
+            await mailer.send();
+
+            // Save the survey & credit count on user
+            await survey.save();
+
+            req.user.credits  -= 1;
+            const user = await req.user.save();
+
+            // Respond with updated user
+            res.send(user);
+        } catch (err) {
+            // Lots of different errors could occur, but using generic 422
+            // 422 : Unprocessable Entity
+            //       Request well-formed but was unable to be followed due to semantic errors
+            res.status(422).send(err);
+        }
     });
 };
